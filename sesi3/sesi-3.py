bf9b26ccfd53d2b87641a6e3fc2901aa22bd76aa
namimod25 <namimod25@gmail.com>
2025-04-18 14:03:21 +0700

    update_sesi3

--- sesi3/sesi-3.py
+++ sesi3/sesi-3.py
@@ -4,6 +4,6 @@ from game import tebak_angka
 
 def main():
         selamat_datang("selamat datang di permainan tebak bintang")
-        
+        tebak_angka.start()
 if __name__ == "__main__":
-       print(main())
+       main()
